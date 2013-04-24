1202db326035a400fc5564c50d518d712ae2ae11
fabrixxm <fabrix.xm@gmail.com>
2013-04-24 06:38:00 -0400

    bump version

--- view/theme/blog/theme.php
+++ view/theme/blog/theme.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Name: Blog
- * Version: 0.5
+ * Version: 0.6
  * Description: Blog theme ported from <a href='http://wordpress.org/extend/themes/twentyeleven'>Twenty Eleven wordpress theme</a>, Licensed under <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU General Public License v2 or later</a>
  * Author: Fabio <http://kirgroup.com/profile/fabrixxm>
  * Maintainer: Fabio <http://kirgroup.com/profile/fabrixxm>
@@ -9,9 +9,7 @@
 
 
 function blog_init(&$a) {
-
     $a->theme_info = array();
-	set_template_engine($a, 'smarty3');
 }
 
 function blog_install(&$a){
